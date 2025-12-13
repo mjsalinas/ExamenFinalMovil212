c93779165a91d4bb5c0fc1013842d113dd92c8b4
Cristiandiaz04 <cristianrojad04@unitec.edu>
2025-12-12 21:03:48 -0600

    prueba2

--- App.tsx
+++ App.tsx
@@ -12,7 +12,7 @@ import StackNavigator from './src/navigation/StackNavigator';
  * (pendiente, en progreso, completado).
  * 
  
- * Esta app utiliza useState y prop drilling INTENCIONALMENTE 
+
  * como antipráctica educativa. Más adelante se refactorizará 
  * para usar Redux/Redux Toolkit.
  * 
